9945c68465115e61467ab455c42a4790145ec20c
monaco89 <nick.monaco15@gmail.com>
2021-09-08 10:37:05 -0400

    define global type

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,8 +1,14 @@
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_ANALYTICS_ID;
 
+declare global {
+  interface Window {
+    gtag: any;
+  }
+}
+
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL): void => {
-  window.gtag("config", GA_TRACKING_ID, {
+  window.gtag('config', GA_TRACKING_ID, {
     page_path: url,
   });
 };
@@ -16,7 +22,7 @@ type GTagEvent = {
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }: GTagEvent): void => {
-  window.gtag("event", action, {
+  window.gtag('event', action, {
     event_category: category,
     event_label: label,
     value,
