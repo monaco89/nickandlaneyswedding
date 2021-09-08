ef75ae5218faec4eb7e0f0a3baff3bb5a6876add
monaco89 <nick.monaco15@gmail.com>
2021-09-08 10:28:38 -0400

    g analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
 import { AppProps } from 'next/app';
-import '../styles/index.css';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+const isProduction = process.env.NODE_ENV === 'production';
+
+const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      /* invoke analytics function only for production */
+      if (isProduction) gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+  // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />;
-}
+};
+
+export default App;
