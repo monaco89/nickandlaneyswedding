c11d40369d9ccb190d7a4e1138d2d7ca67cd57d5
monaco89 <nick.monaco15@gmail.com>
2021-10-30 18:31:31 -0400

    rsvp hero

--- pages/rsvp.tsx
+++ pages/rsvp.tsx
@@ -5,13 +5,18 @@ import Layout from '../components/layout';
 function TheRsvp(): JSX.Element {
   return (
     <Layout title="The Accommodations">
+      <div className="rsvp-hero w-full bg-cover bg-center grayscale filter">
+        <div className="flex items-center justify-end w-full h-full bg-gray-900 bg-opacity-50">
+          <div className="px-10 w-1/3 text-center">
+            <h1 className="text-10xl uppercase">RSVP</h1>
+            <p className="text-2xl">
+              We look forward to seeing you! Please fill out the form below
+            </p>
+          </div>
+        </div>
+      </div>
       <Container>
-        <section className="mb-16 xs:mt-0 mt-16 md:mb-12">
-          <h1 className="text-center text-black font-sans text-7xl font-extralight">
-            RSVP
-          </h1>
-          form here
-        </section>
+        <section className="mb-16 xs:mt-0 mt-16 md:mb-12">form here</section>
       </Container>
     </Layout>
   );
