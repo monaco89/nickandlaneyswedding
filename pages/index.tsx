4655f701afdb80f794a67d707e64dbbd8ca308f5
monaco89 <nick.monaco15@gmail.com>
2021-10-30 17:54:20 -0400

    add images to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import Image from 'next/image';
 import Container from '../components/container';
 import Layout from '../components/layout';
+import pic1 from '../public/assets/home/1.jpg';
+import pic2 from '../public/assets/home/2.jpg';
+import pic3 from '../public/assets/home/3.jpg';
+import pic4 from '../public/assets/home/4.jpg';
+import hashtag from '../public/assets/home/hashtag.png';
 
 function Index(): JSX.Element {
   return (
     <Layout>
       <Container>
-        <section className="mb-16 xs:mt-10 mt-16 md:mb-12">
+        <section className="mb-16 md:mb-12">
           <div className="text-center lg:px-32">
             <h1 className="xs:text-3xl text-6xl">THE WEDDING OF</h1>
             <div className="block xs:h-60 sm:h-80">
@@ -21,6 +27,42 @@ function Index(): JSX.Element {
             <p className="xs:text-3xl text-6xl">August 18th 2023</p>
           </div>
         </section>
+        <div className="grid gap-0 xs:grid-cols-1 grid-cols-4 grid-rows-1 xs:grid-rows-4">
+          <div className="relative w-full h-full">
+            <Image
+              src={pic1}
+              alt="Nick and Delaney holding hands"
+              objectFit="cover"
+            />
+          </div>
+          <div className="relative w-full h-auto">
+            <Image
+              src={pic2}
+              alt="Nick and Delaney sitting"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <div className="relative w-full h-auto">
+            <Image
+              src={pic3}
+              alt="Nick holding Delaney"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <div className="relative w-full h-auto">
+            <Image
+              src={pic4}
+              alt="Nick and Delaney looking at each other"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+        </div>
+        <div className="relative mt-16 w-full h-auto text-center">
+          <Image src={hashtag} alt="#lanetomonaco" width={200} height={200} />
+        </div>
       </Container>
     </Layout>
   );
